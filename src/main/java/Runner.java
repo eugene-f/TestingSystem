e5f004d5657d1e253c4c8c6674965df173cf7550
Yevgeniy Frolov <eugene.frolov.95@gmail.com>
2015-05-02 20:22:48 +0600

    Add logger

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,23 +3,41 @@ import com.frolov.testing.entity.Platform;
 import com.frolov.testing.entity.user.BaseUser;
 import com.frolov.testing.factory.PlatformFactory;
 import org.boon.Boon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Runner {
 
+    private static final Logger logger = LoggerFactory.getLogger(Runner.class);
     public static Platform platform;
 
     public static void main(String[] args) {
+        createPlatformEmulation();
+//        testDao();
+        showLoggerConfig();
+    }
 
-//        System.out.println("Testing System");
-//        platform = PlatformFactory.createPlatform();
-//        System.out.println(Boon.toPrettyJsonWithTypes(platform));
+    private static void showLoggerConfig() {
+        System.out.println("logger.isTraceEnabled(): " + logger.isTraceEnabled());
+        System.out.println("logger.isDebugEnabled(): " + logger.isDebugEnabled());
+        System.out.println("logger.isInfoEnabled(): " + logger.isInfoEnabled());
+        System.out.println("logger.isWarnEnabled(): " + logger.isWarnEnabled());
+        System.out.println("logger.isErrorEnabled(): " + logger.isErrorEnabled());
+    }
 
+    private static void testDao() {
         DaoFactory instance = DaoFactory.getInstance(DaoFactory.Type.Jdbc);
         JdbcUserDao jdbcUserDao = instance.create(JdbcUserDao.class);
         jdbcUserDao.setConnection(jdbcUserDao.getConnection());
         BaseUser byEmail = jdbcUserDao.findByEmail("Ethan_Nash@gmail.com");
         System.out.println(Boon.toPrettyJson(byEmail));
+    }
 
+    private static void createPlatformEmulation() {
+        System.out.println("Testing System");
+        platform = PlatformFactory.createPlatform();
+        System.out.println(Boon.toPrettyJsonWithTypes(platform));
+        logger.info("Platform Emulated");
     }
 
 }
